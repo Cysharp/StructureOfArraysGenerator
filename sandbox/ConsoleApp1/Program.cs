325534563093c8024560020fa6080292e4143b5d
neuecc <ils@neue.cc>
2023-01-24 21:46:52 +0900

    MultiArray done

--- sandbox/ConsoleApp1/Program.cs
+++ sandbox/ConsoleApp1/Program.cs
@@ -31,123 +31,68 @@ public struct Point3D
     public float Z;
 }
 
-internal interface IMultiArray<T>
+[MultiArray(typeof(Point3D))]
+[System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Auto)]
+public readonly partial struct Point3DMultiArray
 {
-    int Length { get; }
-    ReadOnlySpan<byte> GetRawSpan();
-    static abstract int GetByteSize(int length);
-    static abstract T Create(int length, ArraySegment<byte> arrayOffset);
+    // TODO: check index of indexer
 }
 
-[MultiArray(typeof(Point3D))] // SourceGenerator, it will implements below code
-public readonly partial struct Point3DMultiArray : IMultiArray<Point3DMultiArray>
+
+public class Point3DMultiArrayList
 {
-    readonly byte[] __value;
-    readonly int __length;
-    readonly int __byteSize;
-    readonly int __byteOffsetX;
-    readonly int __byteOffsetY;
-    readonly int __byteOffsetZ;
+    Point3DMultiArray array;
+    int length;
 
-    public int Length => __length;
-    public Span<float> X => MemoryMarshal.CreateSpan(ref Unsafe.As<byte, float>(ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(__value), __byteOffsetX)), __length);
-    public Span<float> Y => MemoryMarshal.CreateSpan(ref Unsafe.As<byte, float>(ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(__value), __byteOffsetY)), __length);
-    public Span<float> Z => MemoryMarshal.CreateSpan(ref Unsafe.As<byte, float>(ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(__value), __byteOffsetZ)), __length);
+    public int Length => length;
+    public Span<float> X => array.X.Slice(0, length);
+    public Span<float> Y => array.Y.Slice(0, length);
+    public Span<float> Z => array.Z.Slice(0, length);
 
-    public static int GetByteSize(int length)
+    public Point3DMultiArrayList()
+        : this(4)
     {
-        return Unsafe.SizeOf<float>() * length
-             + Unsafe.SizeOf<float>() * length
-             + Unsafe.SizeOf<float>() * length;
     }
 
-    public static Point3DMultiArray Create(int length, ArraySegment<byte> arrayOffset)
+    public Point3DMultiArrayList(int capacity)
     {
-        return new Point3DMultiArray(length, arrayOffset);
-    }
-
-    public Point3DMultiArray(int length)
-    {
-        this.__byteOffsetX = 0;
-        this.__byteOffsetY = __byteOffsetX + Unsafe.SizeOf<float>() * length;
-        this.__byteOffsetZ = __byteOffsetY + Unsafe.SizeOf<float>() * length;
-
-        this.__byteSize = __byteOffsetZ + Unsafe.SizeOf<float>() * length;
-        this.__value = new byte[__byteSize];
-        this.__length = length;
-    }
-
-    public Point3DMultiArray(int length, ArraySegment<byte> arrayOffset)
-    {
-        this.__byteOffsetX = arrayOffset.Offset;
-        this.__byteOffsetY = __byteOffsetX + Unsafe.SizeOf<float>() * length;
-        this.__byteOffsetZ = __byteOffsetY + Unsafe.SizeOf<float>() * length;
-
-        this.__byteSize = __byteOffsetZ + Unsafe.SizeOf<float>() * length;
-        this.__value = arrayOffset.Array!;
-        this.__length = length;
+        if (capacity < 0) capacity = 1;
+        array = new Point3DMultiArray(capacity);
     }
 
     public Point3D this[int index]
     {
         get
         {
-            ref var x = ref Unsafe.Add(ref Unsafe.As<byte, float>(ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(__value), __byteOffsetX)), index);
-            ref var y = ref Unsafe.Add(ref Unsafe.As<byte, float>(ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(__value), __byteOffsetY)), index);
-            ref var z = ref Unsafe.Add(ref Unsafe.As<byte, float>(ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(__value), __byteOffsetZ)), index);
-            return new Point3D
-            {
-                X = x,
-                Y = y,
-                Z = z
-            };
+            // TODO: check index of indexer
+            return array[index];
         }
         set
         {
-            Unsafe.Add(ref Unsafe.As<byte, float>(ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(__value), __byteOffsetX)), index) = value.X;
-            Unsafe.Add(ref Unsafe.As<byte, float>(ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(__value), __byteOffsetY)), index) = value.Y;
-            Unsafe.Add(ref Unsafe.As<byte, float>(ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(__value), __byteOffsetZ)), index) = value.Z;
+            array[index] = value;
         }
     }
 
-    public ReadOnlySpan<byte> GetRawSpan() => __value.AsSpan(__byteOffsetX, __byteSize);
-
-    public bool SequenceEqual(Point3DMultiArray other)
-    {
-        return GetRawSpan().SequenceEqual(other.GetRawSpan());
-    }
-}
-
-
-public class Point3DMultiList
-{
-    byte[] __value = default!; // TODO:
-    int __length;
-    int __byteSize;
-    int __byteOffsetX;
-    int __byteOffsetY;
-    int __byteOffsetZ;
-
-    public int Length => __length;
-    public Span<float> X => MemoryMarshal.CreateSpan(ref Unsafe.As<byte, float>(ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(__value), __byteOffsetX)), __length);
-    public Span<float> Y => MemoryMarshal.CreateSpan(ref Unsafe.As<byte, float>(ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(__value), __byteOffsetY)), __length);
-    public Span<float> Z => MemoryMarshal.CreateSpan(ref Unsafe.As<byte, float>(ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(__value), __byteOffsetZ)), __length);
-
     public void Add(Point3D value)
     {
-        //var newValue = new byte[
+        // TODO: EnsureCapacity
+        array[length++] = value;
     }
 
     void EnsureCapacity(int newLength)
     {
-        var newArray = new byte[__value.Length * 2];
-
-        // TODO: make nextOffset;
-
-
-        // copy
-
+        var newArray = new Point3DMultiArray(array.Length * 2);
+        array.X.CopyTo(newArray.X);
+        array.Y.CopyTo(newArray.Y);
+        array.Z.CopyTo(newArray.Z);
     }
+
+    //public ReadOnlySpan<byte> GetRawSpan() => __value.AsSpan(__byteOffsetX, __byteSize);
+
+    //public bool SequenceEqual(Point3DMultiArray other)
+    //{
+    //    return GetRawSpan().SequenceEqual(other.GetRawSpan());
+    //}
 }
 
 
@@ -173,31 +118,6 @@ internal sealed class MultiArrayFormatter<T> : MemoryPackFormatter<T>
 
 
 
-partial struct Point3DMultiArray : IMemoryPackable<Point3DMultiArray>
-{
-    public static void RegisterFormatter()
-    {
-        if (!global::MemoryPack.MemoryPackFormatterProvider.IsRegistered<Point3DMultiArray>())
-        {
-            global::MemoryPack.MemoryPackFormatterProvider.Register(new global::MemoryPack.Formatters.MemoryPackableFormatter<Point3DMultiArray>());
-        }
-    }
-
-    public static void Serialize<TBufferWriter>(ref MemoryPackWriter<TBufferWriter> writer, scoped ref Point3DMultiArray value)
-        where TBufferWriter : IBufferWriter<byte>
-    {
-        writer.WriteUnmanaged(value.Length);
-        writer.WriteUnmanagedSpan(value.GetRawSpan());
-    }
-
-    public static void Deserialize(ref MemoryPackReader reader, scoped ref Point3DMultiArray value)
-    {
-        var length = reader.ReadUnmanaged<int>();
-        var array = reader.ReadUnmanagedArray<byte>();
-        value = new Point3DMultiArray(length, array!);
-    }
-}
-
 
 
 
@@ -211,15 +131,8 @@ internal sealed class MultiArrayAttribute2 : Attribute
     {
         this.Type = type;
     }
-
-    public MultiArrayAttribute2(Type type, string[] members)
-    {
-        this.Type = type;
-    }
 }
 
 // TODO: constructor selection?
 // MemoryPack serialization
-
-
 
